ec694f7637447c173f5b34f984baa5716b78ef57
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-29 18:03:47 -0300

    Cria campo para guardar o resultado do AWS Rekognition

--- project/src/core/models.py
+++ project/src/core/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+from django.contrib.postgres.fields import JSONField
 
 
 class AnalysedImage(models.Model):
     image = models.ImageField(upload_to='base/')
+    recokgnition_result = JSONField(default={})
