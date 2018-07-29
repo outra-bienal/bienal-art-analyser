3c220658f1af23b743246c6d8164a1cf7d6c1763
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-29 18:13:14 -0300

    Adiciona campo para guardar o id da tarefa que está processando a imagem

--- project/src/core/models.py
+++ project/src/core/models.py
@@ -4,4 +4,5 @@ from django.contrib.postgres.fields import JSONField
 
 class AnalysedImage(models.Model):
     image = models.ImageField(upload_to='base/')
-    recokgnition_result = JSONField(default={})
+    recokgnition_result = JSONField(default={}, blank=True)
+    job_id = models.CharField(max_length=50, default='', blank=True)
