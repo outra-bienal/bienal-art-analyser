210392f43b3fa73b8d0056b347f00c02826370cf
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-29 18:55:44 -0300

    Utiliza português para o admin

--- project/src/core/models.py
+++ project/src/core/models.py
@@ -1,23 +1,41 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.utils.translation import gettext as _
 
 from proj_utils.redis import RedisAsyncClient
 from src.core.tasks import analyse_image_task
 
 
 class Collection(models.Model):
-    title = models.CharField(max_length=200)
-    date = models.DateField()
+    title = models.CharField(max_length=200, verbose_name=_('Título'))
+    date = models.DateField(verbose_name=_('Data'))
+
+    def run_analysis(self):
+        for image in self.analysed_images.all():
+            if not image.has_all_results:
+                image.enqueue_analysis()
+
+    class Meta:
+        verbose_name = _('Coleção')
+        verbose_name_plural = _('Coleções')
 
 
 class AnalysedImage(models.Model):
-    image = models.ImageField(upload_to='base/')
-    recokgnition_result = JSONField(default={}, blank=True)
-    job_id = models.CharField(max_length=50, default='', blank=True)
-    collection = models.ForeignKey(Collection, related_name='analysed_images', on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='base/', verbose_name=_('Imagem'))
+    recokgnition_result = JSONField(default={}, blank=True, verbose_name=_('AWS Recokgnition'))
+    job_id = models.CharField(max_length=50, default='', blank=True, verbose_name=_('Id job de análise'))
+    collection = models.ForeignKey(Collection, related_name='analysed_images', on_delete=models.CASCADE, verbose_name=_('Coleção'))
+
+    @property
+    def has_all_results(self):
+        return bool(self.recokgnition_result)
 
     def enqueue_analysis(self):
         client = RedisAsyncClient()
         job = client.enqueue_default(analyse_image_task, self.id)
         self.job_id = str(job.id)
         self.save(update_fields=['job_id'])
+
+    class Meta:
+        verbose_name = _('Análise de Imagem')
+        verbose_name_plural = _('Análise de Imagem')
