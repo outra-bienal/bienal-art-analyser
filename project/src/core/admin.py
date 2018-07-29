40d490bd2546687d41e71ddd8fbc90d2d0ebd9da
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-29 18:44:30 -0300

    Adiciona admin para criar e adicionar fotos a uma coleção

--- project/src/core/admin.py
+++ project/src/core/admin.py
@@ -1,5 +1,41 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.utils.translation import gettext as _
 
-from src.core.models import AnalysedImage
+from src.core.models import AnalysedImage, Collection
 
-admin.site.register(AnalysedImage)
+
+class QuestionTagAdmin(admin.ModelAdmin):
+    list_display_links = None
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+
+class AnalysedImageInline(admin.TabularInline):
+    model = AnalysedImage
+    extra = 1
+    suit_classes = 'suit-tab suit-tab-images'
+    fields = ['image', 'collection']
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    suit_form_tabs = (('colecao', _('Coleção')), ('images', _('Imagens')))
+    list_display = ['title', 'date']
+    inlines = [AnalysedImageInline]
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'date'),
+            'classes': ('suit-tab', 'suit-tab-colecao',),
+        }),
+        (_('Imagens'), {
+            'classes': ('suit-tab', 'suit-tab-images',),
+            'fields': []
+        })
+    )
+
+
+
+
+
+admin.site.register(Collection, CollectionAdmin)
