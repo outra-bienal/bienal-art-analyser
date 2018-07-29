a12b3b48cb60354187d6643926e0139fd00b6d1a
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-29 12:57:39 -0300

    Configura teste de settings para utilizar media local

--- project/src/config/test_settings.py
+++ project/src/config/test_settings.py
@@ -4,3 +4,7 @@ TESTING = True
 
 STRING_IF_INVALID = "%%%INVALID_STRING%%%"
 TEMPLATES[0]['OPTIONS']['string_if_invalid'] = STRING_IF_INVALID
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR.child('media')
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
