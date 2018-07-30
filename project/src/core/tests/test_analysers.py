43405ed952885eaf3efb577fcb05140a83252f32
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-30 20:04:05 -0300

    Implementa analyser para o serviço da IBM

--- project/src/core/tests/test_analysers.py
+++ project/src/core/tests/test_analysers.py
@@ -1,10 +1,11 @@
 from unittest.mock import Mock, patch
+from watson_developer_cloud import VisualRecognitionV3
 
 from django.conf import settings
 from django.test import TestCase
 
 from .sample_responses import *
-from src.core.analysers import aws_analyser
+from src.core.analysers import aws_analyser, ibm_analyser
 
 
 class TestAWSAnalyser(TestCase):
@@ -48,5 +49,40 @@ class TestAWSAnalyser(TestCase):
         MockedBoto3.client.return_value = client
 
         data = aws_analyser(self.image_url)
+
+        assert data is None
+
+
+class TestIBMAnalyser(TestCase):
+    image_url = 'https://bienal-image-analyser.s3.amazonaws.com/folder/img.jpg?X-Amz-Algorithm=AWS4-HMAC'
+
+    @patch('src.core.analysers.VisualRecognitionV3')
+    def test_return_response_if_success(self, MockedVisualRecognitionV3):
+        client = Mock(VisualRecognitionV3)
+        client.classify.return_value = IBM_CLASSIFY_RESPONSE
+        MockedVisualRecognitionV3.return_value = client
+
+        clean_url = 'https://bienal-image-analyser.s3.amazonaws.com/folder/img.jpg'
+        data = ibm_analyser(self.image_url)
+        expected = {
+            'main': IBM_CLASSIFY_RESPONSE['images'][0],
+            'faces': {},
+        }
+
+        assert expected == data
+        MockedVisualRecognitionV3.assert_called_once_with(
+            settings.IBM_WATSON_VISUAL_RECOG_VERSION,
+            iam_api_key=settings.IBM_IAM_API_KEY
+        )
+        kwargs = {'url': clean_url}
+        client.classify.assert_called_once_with(**kwargs)
+
+    @patch('src.core.analysers.VisualRecognitionV3')
+    def test_return_none_if_any_error(self, MockedVisualRecognitionV3):
+        client = Mock(VisualRecognitionV3)
+        client.classify.side_effect = Exception
+        MockedVisualRecognitionV3.return_value = client
+
+        data = ibm_analyser(self.image_url)
 
         assert data is None
