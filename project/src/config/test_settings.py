7954722b71c4b30dbd5119825dc9934f26f4c9de
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-30 16:09:37 -0300

    Configura settings da AWS para testes

--- project/src/config/test_settings.py
+++ project/src/config/test_settings.py
@@ -8,3 +8,9 @@ TEMPLATES[0]['OPTIONS']['string_if_invalid'] = STRING_IF_INVALID
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR.child('media')
 DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+AWS_ACCESS_KEY_ID = 'access_key'
+AWS_SECRET_ACCESS_KEY = 'secret_access_key'
+AWS_STORAGE_BUCKET_NAME = 'bucket'
+AWS_S3_HOST = 'host'
+AWS_S3_REGION_NAME = 'region'
