32b047015fdd5aa57164f436fc8c7b779682261d
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-30 20:11:22 -0300

    Implementa tarefa para processar imagem no serviço da IBM

--- project/src/core/tasks.py
+++ project/src/core/tasks.py
@@ -12,3 +12,16 @@ def aws_analyse_image_task(analysed_image_id):
     if result:
         db_image.recokgnition_result = result
         db_image.save(update_fields=['recokgnition_result'])
+
+
+def ibm_analyse_image_task(analysed_image_id):
+    from src.core.models import AnalysedImage
+    try:
+        db_image = AnalysedImage.objects.get(id=analysed_image_id)
+    except AnalysedImage.DoesNotExist:
+        return None
+
+    result = analysers.ibm_analyser(db_image.image.url)
+    if result:
+        db_image.ibm_watson_result = result
+        db_image.save(update_fields=['ibm_watson_result'])
--- project/src/core/tests/test_tasks.py
+++ project/src/core/tests/test_tasks.py
@@ -5,7 +5,7 @@ from django.test import TestCase
 
 from .sample_responses import AWS_LABELS_RESPONSE
 from src.core.models import AnalysedImage
-from src.core.tasks import aws_analyse_image_task
+from src.core.tasks import aws_analyse_image_task, ibm_analyse_image_task
 
 
 class AWSAnalyseImageTast(TestCase):
@@ -37,4 +37,36 @@ class AWSAnalyseImageTast(TestCase):
     def test_fails_silently_if_no_image(self, mocked_analyser):
         self.analysed_image.delete()
         result = aws_analyse_image_task(self.analysed_image.id)
+        assert result is None
+
+
+class IBMAnalyseImageTast(TestCase):
+
+    def setUp(self):
+        self.analysed_image = mommy.make(AnalysedImage, _create_files=True)
+
+    @patch('src.core.tasks.analysers.ibm_analyser')
+    def test_populate_ibm_watson_result_with_the_output(self, mocked_analyser):
+        mocked_analyser.return_value = {'some': 'data'}
+
+        ibm_analyse_image_task(self.analysed_image.id)
+        self.analysed_image.refresh_from_db()
+
+        assert self.analysed_image.ibm_watson_result == {'some': 'data'}
+        mocked_analyser.assert_called_once_with(self.analysed_image.image.url)
+
+    @patch('src.core.tasks.analysers.ibm_analyser')
+    def test_do_not_update_if_no_result(self, mocked_analyser):
+        mocked_analyser.return_value = None
+
+        ibm_analyse_image_task(self.analysed_image.id)
+        self.analysed_image.refresh_from_db()
+
+        assert self.analysed_image.ibm_watson_result == {}
+        mocked_analyser.assert_called_once_with(self.analysed_image.image.url)
+
+    @patch('src.core.tasks.analysers.ibm_analyser')
+    def test_fails_silently_if_no_image(self, mocked_analyser):
+        self.analysed_image.delete()
+        result = ibm_analyse_image_task(self.analysed_image.id)
         assert result is None
