d841cfd67e83b2cc7b6d9ef18a086fe12f5e7772
Bernardo Fontes <bernardoxhc@gmail.com>
2018-07-31 04:12:47 -0300

    Cria tarefa para rodar análise do google

--- project/src/core/tasks.py
+++ project/src/core/tasks.py
@@ -25,3 +25,16 @@ def ibm_analyse_image_task(analysed_image_id):
     if result:
         db_image.ibm_watson_result = result
         db_image.save(update_fields=['ibm_watson_result'])
+
+
+def google_analyse_image_task(analysed_image_id):
+    from src.core.models import AnalysedImage
+    try:
+        db_image = AnalysedImage.objects.get(id=analysed_image_id)
+    except AnalysedImage.DoesNotExist:
+        return None
+
+    result = analysers.google_analyser(db_image.image.url)
+    if result:
+        db_image.google_vision_result = result
+        db_image.save(update_fields=['google_vision_result'])
