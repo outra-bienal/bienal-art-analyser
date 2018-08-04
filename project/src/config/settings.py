cf47d8fd51c1cba6a419b9250423d5d6f6027dbf
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-04 00:47:55 -0300

    Torna configurações mais específicas do darknet como opicional

--- project/src/config/settings.py
+++ project/src/config/settings.py
@@ -138,9 +138,9 @@ AZURE_VISION_API_KEY = config('AZURE_VISION_API_KEY', default='missing_key')
 
 #YOLO config
 DARKNET_DIR = config('DARKNET_DIR', cast=Path)
-DARKNET_BIN = config('DARKNET_BIN')
-YOLO_CONF = config('YOLO_CONF')
-YOLO_WEIGHTS = config('YOLO_WEIGHTS')
+DARKNET_BIN = config('DARKNET_BIN', default=DARKNET_DIR.child('darknet'))
+YOLO_CONF = config('YOLO_CONF', default=DARKNET_DIR.child('cfg', 'yolov3.cfg'))
+YOLO_WEIGHTS = config('YOLO_WEIGHTS', default=DARKNET_DIR.child('yolov3.weights'))
 
 import django_heroku
 django_heroku.settings(locals())
