4c636c3e8b8559d040c2abb3f1a8716ec031f189
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-08 18:50:01 -0300

    Corrige script

--- project/src/core/management/commands/load_detectron_results.py
+++ project/src/core/management/commands/load_detectron_results.py
@@ -12,24 +12,21 @@ class Command(BaseCommand):
     help = _('Cria coleção e faz upload de arquivos no diretório')
 
     def add_arguments(self, parser):
-        parser.add_argument('titulo_colecao', type=str)
         parser.add_argument('nome_diretorio_imagens', type=str)
 
     def handle(self, *args, **kwargs):
-        collection_title = kwargs['titulo_colecao']
         images_dir = settings.LOAD_COLLECTIONS_DIR.child(kwargs['nome_diretorio_imagens'])
 
         if not images_dir.exists():
             raise CommandError(_('Dir {} não exite.').format(images_dir))
         if not images_dir.isdir():
             raise CommandError(_('{} não é um diretório.').format(images_dir))
-        if Collection.objects.filter(title=collection_title).exists():
-            raise CommandError(_('A coleção {} já existe.').format(collection_title))
 
-        print("Creating collection...")
-        collection = Collection.objects.create(title=collection_title, date=date.today())
-        print("Uploading images...")
+        print("Atualizando imagens da análise do Detectron...")
         for image_file in tqdm(images_dir.listdir()):
-            analysed_image = AnalysedImage(collection=collection)
-            analysed_image.write_image_field(image_file)
-            analysed_image.save()
+            f_name = image_file.name.split('.')[0]
+            image_prefix = "{}/{}".format(AnalysedImage.BASE_UPLOAD, f_name)
+            image = AnalysedImage.objects.get(image__startswith=image_prefix)
+            image.write_detectron_field(image_file)
+            image.save()
+
