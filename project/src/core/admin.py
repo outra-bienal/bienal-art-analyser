b11bf2ae10995056e772071113d0abc7f021b3df
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-20 19:29:37 -0300

    Exposes data

--- project/src/core/admin.py
+++ project/src/core/admin.py
@@ -67,8 +67,8 @@ class CollectionAdmin(admin.ModelAdmin):
 class AnalysedImageAdmin(admin.ModelAdmin):
     list_display = ['id', 'preview_list', 'processed', 'link_to_collection']
     list_filter = ['collection__title']
-    exclude = ['collection', 'image', 'recokgnition_result', 'recokgnition_job_id', 'ibm_watson_result', 'ibm_watson_job_id', 'deep_ai_result', 'deep_ai_job_id', 'google_vision_result', 'google_vision_job_id', 'azure_vision_result', 'azure_vision_job_id', 'yolo_image', 'yolo_job_id', 'detectron_image', 'clarifai_result', 'clarifai_job_id']
-    readonly_fields = ['link_to_collection', 'preview', 'yolo', 'detectron', 'aws', 'ibm', 'google', 'azure', 'deep_ai', 'clarifai']
+    exclude = ['collection', 'image', 'recokgnition_result', 'recokgnition_job_id', 'ibm_watson_result', 'ibm_watson_job_id', 'deep_ai_result', 'deep_ai_job_id', 'google_vision_result', 'google_vision_job_id', 'azure_vision_result', 'azure_vision_job_id', 'yolo_image', 'yolo_job_id', 'detectron_image', 'clarifai_result', 'clarifai_job_id', 'dense_cap_job_id']
+    readonly_fields = ['link_to_collection', 'preview', 'yolo', 'detectron', 'dense_cap_image', 'dense_cap_full_image', 'aws', 'ibm', 'google', 'azure', 'deep_ai', 'clarifai']
 
     def has_add_permission(self, request):
         return False
