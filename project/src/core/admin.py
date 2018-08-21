215a11bbee238a8a77c20e97ed5f128bc8dfb1b6
Bernardo Fontes <bernardoxhc@gmail.com>
2018-08-21 12:37:42 -0300

    Display dense cap images

--- project/src/core/admin.py
+++ project/src/core/admin.py
@@ -74,8 +74,8 @@ class CollectionAdmin(admin.ModelAdmin):
 class AnalysedImageAdmin(admin.ModelAdmin):
     list_display = ['id', 'preview_list', 'processed', 'link_to_collection']
     list_filter = ['collection__title']
-    exclude = ['collection', 'image', 'recokgnition_result', 'recokgnition_job_id', 'ibm_watson_result', 'ibm_watson_job_id', 'deep_ai_result', 'deep_ai_job_id', 'google_vision_result', 'google_vision_job_id', 'azure_vision_result', 'azure_vision_job_id', 'yolo_image', 'yolo_job_id', 'detectron_image', 'clarifai_result', 'clarifai_job_id', 'dense_cap_job_id']
-    readonly_fields = ['link_to_collection', 'preview', 'yolo', 'detectron', 'dense_cap_image', 'dense_cap_full_image', 'aws', 'ibm', 'google', 'azure', 'deep_ai', 'clarifai']
+    exclude = ['collection', 'image', 'recokgnition_result', 'recokgnition_job_id', 'ibm_watson_result', 'ibm_watson_job_id', 'deep_ai_result', 'deep_ai_job_id', 'google_vision_result', 'google_vision_job_id', 'azure_vision_result', 'azure_vision_job_id', 'yolo_image', 'yolo_job_id', 'detectron_image', 'clarifai_result', 'clarifai_job_id', 'dense_cap_job_id', 'dense_cap_image', 'dense_cap_full_image']
+    readonly_fields = ['link_to_collection', 'preview', 'yolo', 'detectron', 'dense_cap', 'dense_cap_full', 'aws', 'ibm', 'google', 'azure', 'deep_ai', 'clarifai']
 
     def has_add_permission(self, request):
         return False
@@ -97,6 +97,16 @@ class AnalysedImageAdmin(admin.ModelAdmin):
     def yolo(self, obj):
         if obj.yolo_image:
             return preview(obj.yolo_image.url, "50em")
+        return '---'
+
+    def dense_cap(self, obj):
+        if obj.dense_cap_image:
+            return preview(obj.dense_cap_image.url, "50em")
+        return '---'
+
+    def dense_cap_full(self, obj):
+        if obj.dense_cap_full_image:
+            return preview(obj.dense_cap_full_image.url, "50em")
         return '---'
 
     def detectron(self, obj):
